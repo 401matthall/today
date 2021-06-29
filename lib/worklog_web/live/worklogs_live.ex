fb2f7e2e23c65770a157c7960d7294d0ed634fbd
Matt Hall <401matthall@gmail.com>
2021-06-29 16:21:51 -0400

    Added function to persist worklogs from front end

--- lib/worklog_web/live/worklogs_live.ex
+++ lib/worklog_web/live/worklogs_live.ex
@@ -1,9 +1,34 @@
 defmodule TodayWeb.WorklogsLive do
   use TodayWeb, :live_view
+  require Logger
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, session, socket) do
+    if connected?(socket) do
+    end
+
+    {
+      :ok,
+      socket
+        |> assign(current_user: session["current_user"])
+        |> assign(session: session)
+    }
+  end
+
+  @impl true
+    def handle_event("create_worklog", params, socket) do
+    worklog = params["worklog"]
+    |> Map.put("user_id", socket.assigns.current_user)
+    case persist_worklog(worklog) do
+      {:ok, _} -> {:noreply, put_flash(socket, :info, "Saved!")}
+      {:error, %Ecto.Changeset{}} -> {:noreply, put_flash(socket, :error, "An error has occured while trying to save worklog.")}
+    end
+  end
+
+  defp persist_worklog(worklog_params = %{}) do
+    worklog = %Today.Worklog{}
+    changeset = Today.Worklog.changeset(worklog, %{title: worklog_params["title"], body: worklog_params["body"], user_id: worklog_params["user_id"]})
+    Today.Repo.insert(changeset)
   end
 
 end
