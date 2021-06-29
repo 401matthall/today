7ad9619777232195575d5fdb83aaacb3df2ad105
Matt Hall <401matthall@gmail.com>
2021-06-29 16:20:34 -0400

    Moved front page to Dashboard

---
+++ lib/worklog_web/live/dashboard_live.ex
@@ -0,0 +1,24 @@
+defmodule TodayWeb.DashboardLive do
+  use TodayWeb, :live_view
+  require Logger
+  require Ecto.Query
+
+  @impl true
+  def mount(_params, session, socket) do
+    if connected?(socket) do
+    end
+
+    worklogs = Today.Worklog
+      |> Ecto.Query.where(user_id: ^session["current_user"])
+      |> Today.Repo.all
+
+    {
+      :ok,
+      socket
+        |> assign(current_user: session["current_user"])
+        |> assign(session: session)
+        |> assign(worklogs: worklogs)
+    }
+  end
+
+end
