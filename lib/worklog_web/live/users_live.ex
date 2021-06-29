70924adf8a40e9b1655603a3c04f4149ccc5bbeb
Matt Hall <401matthall@gmail.com>
2021-06-29 12:57:44 -0400

    Stubbed liveviews for tags, users, worklogs.

---
+++ lib/worklog_web/live/users_live.ex
@@ -0,0 +1,9 @@
+defmodule TodayWeb.UsersLive do
+  use TodayWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+end
---
+++ lib/worklog_web/live/worklogs_live.ex
@@ -0,0 +1,9 @@
+defmodule TodayWeb.WorklogsLive do
+  use TodayWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+end
