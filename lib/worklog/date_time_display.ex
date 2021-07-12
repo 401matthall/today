8f7d7c08e57f8573a33c608594e4f35f56ead2af
Matt Hall <401matthall@gmail.com>
2021-07-12 08:30:33 -0400

    pretty time formatting

--- lib/worklog/date_time_display.ex
+++ lib/worklog/date_time_display.ex
@@ -8,4 +8,12 @@ defmodule Today.DateTimeDisplay do
       {:error, message} -> Logger.info(message); date;
     end
   end
+
+  def format_pretty_datetime(date, timezone) do
+    case date |> Timex.Timezone.convert(timezone) |> Timex.format("{WDfull}, {Mfull} {D} {YYYY} {h12}:{m}:{s} {am} {Zabbr}") do
+      {:ok, formatted_date} -> formatted_date
+      {:error, {error_type, message}} -> Logger.info(error_type); Logger.info(message); date;
+      {:error, message} -> Logger.info(message); date;
+    end
+  end
 end
