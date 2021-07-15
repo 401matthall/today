a8156651cec0b773c3958756bfda256450aa3599
Matt Hall <401matthall@gmail.com>
2021-07-15 10:30:55 -0400

    I honestly don't know what this first parameter even does.

--- priv/repo/migrations/20210705135501_add_index_text_user_id.exs
+++ priv/repo/migrations/20210705135501_add_index_text_user_id.exs
@@ -2,6 +2,6 @@ defmodule Today.Repo.Migrations.CreateTags do
   use Ecto.Migration
 
   def change do
-    create index(:tags, [:text, :user_id], name: :tags_text_user_id_index, unique: true)
+    create index(:tags_text_user_id, [:text, :user_id], name: :tags_text_user_id_index, unique: true)
   end
 end
