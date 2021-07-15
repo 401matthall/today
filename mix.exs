1001d9792b1085e9507937a2c71e40d2c1377599
Matt Hall <401matthall@gmail.com>
2021-07-15 17:35:26 -0400

    update applications

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule Today.MixProject do
   def application do
     [
       mod: {Today.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext, :phoenix_ecto, :postgrex, :ssl],
+      extra_applications: [:logger, :runtime_tools],
     ]
   end
 
