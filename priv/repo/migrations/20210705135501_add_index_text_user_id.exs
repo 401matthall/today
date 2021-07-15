9b3e75cb3c0621529aa1233b7000bb89d4b06857
Matt Hall <401matthall@gmail.com>
2021-07-15 11:01:31 -0400

    I'm an idiot... This was all about the serial number of the migrations.

--- priv/repo/migrations/20210705135501_add_index_text_user_id.exs
+++ priv/repo/migrations/20210705135501_add_index_text_user_id.exs
@@ -1,8 +1,7 @@
 defmodule Today.Repo.Migrations.CreateTags do
   use Ecto.Migration
-  alias Today.Tags
 
   def change do
-    create index(Tags, [:text, :user_id], name: :tags_text_user_id_index, unique: true)
+    create index(:tags, [:text, :user_id], name: :tags_text_user_id_index, unique: true)
   end
 end
