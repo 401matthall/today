9e5bc3f14aa9afeb863266f43ca32f34fb216913
Matt Hall <401matthall@gmail.com>
2021-07-15 10:38:41 -0400

    Table... It's the table... Which maybe ecto can pickup from the Module?

--- priv/repo/migrations/20210705135501_add_index_text_user_id.exs
+++ priv/repo/migrations/20210705135501_add_index_text_user_id.exs
@@ -1,7 +1,8 @@
 defmodule Today.Repo.Migrations.CreateTags do
   use Ecto.Migration
+  alias Today.Tags
 
   def change do
-    create index(:tags_text_user_id, [:text, :user_id], name: :tags_text_user_id_index, unique: true)
+    create index(Tags, [:text, :user_id], name: :tags_text_user_id_index, unique: true)
   end
 end
